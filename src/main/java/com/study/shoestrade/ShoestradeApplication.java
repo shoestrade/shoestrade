a8a45e3f5e8a37bbee1b97696a07c592ac415381
0takkk <https://github.com/0takkk>
2022-03-25 21:20:12 +0900

    project setting

---
+++ src/main/java/com/study/shoestrade/ShoestradeApplication.java
@@ -0,0 +1,13 @@
+package com.study.shoestrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoestradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoestradeApplication.class, args);
+	}
+
+}
