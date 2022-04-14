5cbda25e5981db831fc1b41c89a96ef417908588
jscheon <cjswltjr159@naver.com>
2022-04-14 23:12:18 +0900

    refactor : EnableJpaAuditing  추가

--- src/main/java/com/study/shoestrade/domain/BaseEntity.java
+++ src/main/java/com/study/shoestrade/domain/BaseEntity.java
@@ -5,13 +5,15 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
-@EntityListeners(AuditingEntityListener.class)
-@MappedSuperclass
 @Getter
-public class BaseEntity {
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
 
     @CreatedDate
     @Column(updatable = false)
@@ -19,16 +21,4 @@ public class BaseEntity {
 
     @LastModifiedDate
     private LocalDateTime lastModifiedDate;
-
-    @PrePersist
-    public void prePersist() {
-        LocalDateTime now = LocalDateTime.now();
-        createdDate = now;
-        lastModifiedDate = now;
-    }
-
-    @PreUpdate
-    public void preUpdate(){
-        lastModifiedDate = LocalDateTime.now();
-    }
 }
