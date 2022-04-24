3302f4614896a03df531558edd1af8244807e4a9
0takkk <https://github.com/0takkk>
2022-04-24 22:08:00 +0900

    add : 관리자 회원 상세 정보 조회

--- src/main/java/com/study/shoestrade/repository/member/MemberRepositoryCustom.java
+++ src/main/java/com/study/shoestrade/repository/member/MemberRepositoryCustom.java
@@ -4,6 +4,10 @@ import com.study.shoestrade.domain.member.Member;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.Optional;
+
 public interface MemberRepositoryCustom {
     Page<Member> findMembers(String email, Pageable pageable);
+
+    Optional<Member> findMemberDetail(Long id);
 }
